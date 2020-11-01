010362c81c539e3fb296f1ac27264074e3cba8f8
Ville Turppo <villetur@uef.fi>
2020-11-01 21:49:33 +0200

    data avattu

--- Data/create_learning2020.R
+++ Data/create_learning2020.R
@@ -1,1 +1,3 @@
 # Ville Turppo, 1.11.2020, This is the new Rscript for week 2.
+week2 <- read.delim("https://www.mv.helsinki.fi/home/kvehkala/JYTmooc/JYTOPKYS3-data.txt", header = TRUE)
+head(week2)

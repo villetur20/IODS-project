63aa3f3c2a4adec0130b5976818202410a6fce91
Ville Turppo <villetur@uef.fi>
2020-11-23 18:47:37 +0200

    analysis 1. push

--- Data/create_human.R
+++ Data/create_human.R
@@ -60,3 +60,55 @@ dim(hd_gii)
 
 getwd()
 write.table(hd_gii, file = "C:/Users/ville/OneDrive/Työpöytä/Open science kurssi 2020/IODS-project/data/human.csv")
+
+
+
+
+# Week 5 Datawrangling
+
+##reading the data via internet link and exploring structure and dimension.
+
+x <- read.table("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt", sep  =",", header = T)
+str(x)
+dim(x)
+## There are 195 observations and 19 variables. Human development index (HDI)= index calculated using life expectancy, schooling and gross national income (GNI).
+## This index takes in account more variables than just economic growth.
+## Data includes also variables for work participation, amount of females in parliament, birth and mortality ratios.
+
+## accessing libraries.
+library(tidyr)
+library(stringr)
+library(dplyr)
+str(x$GNI)
+
+# 1) Transformin GNI variable
+str_replace(x$GNI, pattern=",", replace ="")
+
+# 2) Removing unwanted varibales.
+keepx <- c("Country", "Edu2.FM", "Labo.FM", "Life.Exp", "Edu.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+x <- dplyr::select(x, one_of(keepx))
+
+# 3) Removing rows with missing values. 
+complete.cases(x)
+data.frame(x[-1], comp = complete.cases(x))
+x1 <- filter(x, complete.cases(x))
+
+# 4) Excluding observations which are related to regions. As seen in results, the last 7 are combinations, not countries.
+tail(x1, 10)
+# "last"= last variable to keep
+last1 <- nrow(x1) - 7
+last1
+x1_ <- x1[1:last1, ]
+x1_
+
+# 5) Countries to rownames and then removing country column. "Ready" = data without country column.
+
+rownames(x1_) <- x1_$Country
+x1_
+Ready = subset(x1_, select = -Country)
+## "dim(Ready)" 155 obs and 8 variables.
+Ready
+dim(Ready)
+
+## Saving the "Ready" to human.csv and overwriting the old data.
+write.table(Ready, file = "C:/Users/ville/OneDrive/Työpöytä/Open science kurssi 2020/IODS-project/data/human.csv", row.names = TRUE)
